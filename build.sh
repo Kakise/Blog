09409fdfe332a5153d07c3d3e0085bee7cc90731
Kakise <me@kakise.xyz>
2018-05-08 08:10:10 +0000

    Update 7.2.1: Fixed wrong path during build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Change variables before building
 sed -i s/ONESIGNAL_APP_KEY/$ONESIGNAL_APP_KEY/g _config.yml
-sed -i s/ONESIGNAL_APP_AUTH_KEY/$ONESGINAL_APP_AUTH_KEY/g _config.yml
+sed -i s/ONESIGNAL_APP_AUTH_KEY/$ONESIGNAL_APP_AUTH_KEY/g _config.yml
 
 # Deploy the site
 hexo deploy
