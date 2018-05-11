646784c82f4e01edb9f47dec022c951a50cd896a
Kakise <me@kakise.xyz>
2018-05-11 12:28:10 +0000

    Fixed: Python script path

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sed -i s/ONESIGNAL_APP_KEY/$ONESIGNAL_APP_KEY/g _config.yml
 sed -i s/ONESIGNAL_APP_AUTH_KEY/$ONESIGNAL_APP_AUTH_KEY/g _config.yml
 
 # Change navigation
-python3 ./scripts/nav.py
+python3 scripts/nav.py
 
 # Deploy the site
 hexo deploy
