93614de833b71325a54c394fa05535ab9a090458
Kakise <me@kakise.xyz>
2018-05-11 13:42:43 +0000

    Fixes #39

--- nav.py
+++ nav.py
@@ -6,9 +6,13 @@ nav = json.load(json_data)
 json_data.close()
 
 string = "nav:\n"
+string2 = "urls:\n"
 
 for item in nav['nav']:
     string += "  " + item["link"] + ": " + item["url"] + "\n"
+    string2 += "- " + item["url"] + "\n"
 
 for line in fileinput.input("./themes/cactus/_config.yml", inplace=True):
     print (line.replace("NAV_PLACEHOLDER", string),)
+for line in fileinput.input("./_config.yml", inplace=True):
+    print (line.replace("URL_PLACEHOLDER", string2),)
