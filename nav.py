e22fb71ea6b1527792c7d5f1c5d044d63ae1badb
Kakise <me@kakise.xyz>
2018-05-12 07:13:14 +0000

    Changed line replacing method

--- nav.py
+++ nav.py
@@ -13,7 +13,15 @@ for item in nav['nav']:
     string += "  " + item["link"] + ": " + item["url"] + "\n"
     string2 += "      - " + item["url"] + "\n"
 
-for line in fileinput.input("./themes/cactus/_config.yml", inplace=True):
-    print (line.replace("NAV_PLACEHOLDER", string))
-for line in fileinput.input("./_config.yml", inplace=True):
-    print (line.replace("haha: URL_PLACEHOLDER", string2))
+#for line in fileinput.input("./themes/cactus/_config.yml", inplace=True):
+#    print (line.replace("NAV_PLACEHOLDER", string),)
+#for line in fileinput.input("./_config.yml", inplace=True):
+#    print (line.replace("haha: URL_PLACEHOLDER", string2),)
+
+text = open("./themes/cactus/_config.yml", "r").read()
+text = text.replace("NAV_PLACEHOLDER", string) 
+open("./themes/cactus/_config.yml", "w").write(text)
+
+text = open("./_config.yml", "r").read()
+text = text.replace("urls: URL_PLACEHOLDER", string2) 
+open("./_config.yml", "w").write(text)
