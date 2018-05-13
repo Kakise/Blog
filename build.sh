ce619764bd373df6ccec2bb4d8b28aeb393b5d74
Labo Member 004 <mail@kakise.me>
2018-05-13 17:16:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ sed -i s/ONESIGNAL_APP_AUTH_KEY/$ONESIGNAL_APP_AUTH_KEY/g _config.yml
 sed -i s/REPO_PLACEHOLDER/$REPO/g source/admin/config.yml
 
 # Change navigation
-pip3 install pyyaml
+pip install pyyaml
 python3 nav.py
 
 # Deploy the site
