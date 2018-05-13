e567e2de43f2bcff45068cb1b0bc272de3d2a293
Labo Member 004 <mail@kakise.me>
2018-05-13 17:13:20 +0200

    Update nav.py

--- nav.py
+++ nav.py
@@ -1,10 +1,15 @@
 import json
+import yaml
 import fileinput
 import subprocess
 
 json_data=open("./source/_data/nav.json")
 nav = json.load(json_data)
 json_data.close()
+
+yml=open("./_config.ejs")
+conf = yaml.load(yml)
+yml.close()
 
 string = "nav:\n"
 string2 = "urls:\n"
@@ -13,11 +18,6 @@ for item in nav['nav']:
     string += "  " + item["link"] + ": " + item["url"] + "\n"
     string2 += "      - " + item["url"] + "\n"
 
-#for line in fileinput.input("./themes/cactus/_config.yml", inplace=True):
-#    print (line.replace("NAV_PLACEHOLDER", string),)
-#for line in fileinput.input("./_config.yml", inplace=True):
-#    print (line.replace("haha: URL_PLACEHOLDER", string2),)
-
 text = open("./themes/cactus/_config.yml", "r").read()
 text = text.replace("NAV_PLACEHOLDER", string) 
 open("./themes/cactus/_config.yml", "w").write(text)
@@ -25,3 +25,7 @@ open("./themes/cactus/_config.yml", "w").write(text)
 text = open("./_config.yml", "r").read()
 text = text.replace("urls: URL_PLACEHOLDER", string2) 
 open("./_config.yml", "w").write(text)
+
+text = open("./themes/cactus/layout/index.ejs", "r").read()
+text = text.replace("URL_PLACEHOLDER", conf['url']) 
+open("./themes/cactus/layout/index.ejs", "w").write(text)
