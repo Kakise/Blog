ae0bff7cf18c1b5ff60478457a2a7a51f3f06d55
Kakise <me@kakise.xyz>
2018-05-21 01:12:25 +0000

    Some random fixes

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@ REPO=(${REPOSITORY_URL//:/ })
 echo ${REPO[1]}
 echo $URL
 echo $CONTEXT
+echo $PULL_REQUEST
 
 # Change variables before building
-sed -i s/ONESIGNAL_APP_KEY/$ONESIGNAL_APP_KEY/g _config.yml
-sed -i s/ONESIGNAL_APP_AUTH_KEY/$ONESIGNAL_APP_AUTH_KEY/g _config.yml
-sed -i s/SITEURL/$URL/g _config.yml
-sed -i s/REPO_PLACEHOLDER/${REPO[1]}/g source/admin/config.yml
-sed -i s/SITEURL_PLACEHOLDER/$URL/g source/admin/config.yml
+sed -i s/ONESIGNAL_APP_KEY/"$ONESIGNAL_APP_KEY"/g _config.yml
+sed -i s/ONESIGNAL_APP_AUTH_KEY/"$ONESIGNAL_APP_AUTH_KEY"/g _config.yml
+sed -i s/SITEURL/"$URL"/g _config.yml
+sed -i s/REPO_PLACEHOLDER/"${REPO[1]}"/g source/admin/config.yml
+sed -i s/SITEURL_PLACEHOLDER/"$URL"/g source/admin/config.yml
 
 # Change navigation
 pip3 install pyyaml
