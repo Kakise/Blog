a2679fa8fe6872f278145fbbfdf3bfb1ee388bd2
Labo Member 004 <mail@kakise.me>
2018-05-21 02:41:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+REPO=(${REPOSITORY_URL//:/ })[1]
+
 # Change variables before building
 sed -i s/ONESIGNAL_APP_KEY/$ONESIGNAL_APP_KEY/g _config.yml
 sed -i s/ONESIGNAL_APP_AUTH_KEY/$ONESIGNAL_APP_AUTH_KEY/g _config.yml
 sed -i s/REPO_PLACEHOLDER/$REPO/g source/admin/config.yml
-
-echo $REPOSITORY_URL
 
 # Change navigation
 pip3 install pyyaml
