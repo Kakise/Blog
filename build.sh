54932655c704839777be08fad20649aa45818f85
Kakise <me@kakise.xyz>
2018-05-21 00:56:53 +0000

    Various changes to building

--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 REPO=(${REPOSITORY_URL//:/ })[1]
 
 # Change variables before building
 sed -i s/ONESIGNAL_APP_KEY/$ONESIGNAL_APP_KEY/g _config.yml
 sed -i s/ONESIGNAL_APP_AUTH_KEY/$ONESIGNAL_APP_AUTH_KEY/g _config.yml
+sed -i s/SITEURL/$URL/g _config.yml
 sed -i s/REPO_PLACEHOLDER/$REPO/g source/admin/config.yml
+sed -i s/SITEURL_PLACEHOLDER/$URL/g source/admin/config.yml
 
 # Change navigation
 pip3 install pyyaml
 python nav.py
 
 # Deploy the site
-hexo deploy
+if [ $CONTEXT ="production" ]
+then
+    hexo deploy
+else
+    hexo generate
+fi
