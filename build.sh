949adf833ff96d4bd388e99b04f3af83492aa9bc
Kakise <me@kakise.xyz>
2018-05-21 01:17:04 +0000

    Some random fixes

--- build.sh
+++ build.sh
@@ -7,18 +7,18 @@ echo $CONTEXT
 echo $PULL_REQUEST
 
 # Change variables before building
-sed -i s/ONESIGNAL_APP_KEY/"$ONESIGNAL_APP_KEY"/g _config.yml
-sed -i s/ONESIGNAL_APP_AUTH_KEY/"$ONESIGNAL_APP_AUTH_KEY"/g _config.yml
-sed -i s/SITEURL/"$URL"/g _config.yml
-sed -i s/REPO_PLACEHOLDER/"${REPO[1]}"/g source/admin/config.yml
-sed -i s/SITEURL_PLACEHOLDER/"$URL"/g source/admin/config.yml
+sed -i s@ONESIGNAL_APP_KEY@"$ONESIGNAL_APP_KEY"@g _config.yml
+sed -i s@ONESIGNAL_APP_AUTH_KEY@"$ONESIGNAL_APP_AUTH_KEY"@g _config.yml
+sed -i s@SITEURL@"$URL"@g _config.yml
+sed -i s@REPO_PLACEHOLDER@"${REPO[1]}"@g source@admin@config.yml
+sed -i s@SITEURL_PLACEHOLDER@"$URL"@g source@admin@config.yml
 
 # Change navigation
 pip3 install pyyaml
 python nav.py
 
 # Deploy the site
-if [[ $CONTEXT = "production" ]]
+if [[ $CONTEXT = "production" && $PULL_REQUEST == "true" ]]
 then
     hexo deploy
 else
