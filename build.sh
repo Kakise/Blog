3de1b645df5bc753cbe0f86f0cb595b99d45af2a
Labo Member 004 <mail@kakise.me>
2018-05-21 02:38:09 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ sed -i s/ONESIGNAL_APP_KEY/$ONESIGNAL_APP_KEY/g _config.yml
 sed -i s/ONESIGNAL_APP_AUTH_KEY/$ONESIGNAL_APP_AUTH_KEY/g _config.yml
 sed -i s/REPO_PLACEHOLDER/$REPO/g source/admin/config.yml
 
+echo $REPOSITORY_URL
+
 # Change navigation
 pip3 install pyyaml
 python nav.py
