2593a90775a61ea5648bc3fe00fae096ddceaa95
Kakise <mail@kakise.me>
2019-05-20 23:11:00 +0200

    test-purify

---
+++ purify.js
@@ -0,0 +1,44 @@
+const purify = require("purify-css")
+var content = []
+var options = {
+    output: 'public/src/style-clean.css'
+};
+
+var path = require('path'), fs = require('fs');
+
+function fromDir(startPath, filter, callback) {
+
+    //console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)) {
+        console.log("no dir ", startPath);
+        return;
+    }
+
+    var files = fs.readdirSync(startPath);
+    for (var i = 0; i < files.length; i++) {
+        var filename = path.join(startPath, files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            fromDir(filename, filter, callback); //recurse
+        } else if (filter.test(filename)) callback(filename);
+    }
+    ;
+};
+
+fromDir('./public', /\.html$/, function (filename) {
+    console.log('-- found: ', filename);
+    content.push("./" + filename);
+});
+
+fromDir('./public', /\.js$/, function (filename) {
+    console.log('-- found: ', filename);
+    content.push("./" + filename);
+});
+
+fromDir('./public', /\.css$/, function (filename) {
+    console.log('-- found: ', filename);
+    content.push("./" + filename);
+});
+
+purify(content, "", options);
